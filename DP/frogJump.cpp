35bbab1b9b7b24a534c6270807b01232991c0e34
Vinni Kapoor <vinni3871@gmail.com>
2025-11-06 21:10:55 +0530

    Add files

---
+++ DP/frogJump.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <cmath>     // for abs()
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int frogJump(vector<int>& heights) {
+
+        int n = heights.size();
+
+        vector<int> dp(n, 0);
+        dp[0] = 0;
+
+        for(int i = 1; i < heights.size(); i++) {
+            int step1_Energy = dp[i-1] + abs(heights[i] - heights[i-1]);
+            int step2_Energy = INT_MAX;
+            if(i > 1) {
+                int step2_Energy = dp[i-2] + abs(heights[i] - heights[i-2]);
+            }
+
+            dp[i] = min(step1_Energy, step2_Energy);
+            
+        }
+
+        return dp[n-1];
+    }
+};
---
+++ DP/frogJumpWithkDistances.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int frogJump(vector<int>& heights, int k) {
+
+        int n = heights.size();
+
+        vector<int> dp(n, INT_MAX);
+        dp[0] = 0;
+
+        for(int i = 1; i < heights.size(); i++) {
+            for(int j = 1; j <= k; j++) {
+                if(i-j >= 0) {
+                    int jumpEnergy = dp[i-j] + abs(heights[i] - heights[j]);
+                    dp[i] = min(dp[i], jumpEnergy);
+                }
+            }
+        }
+
+        return dp[n-1];
+    }
+};
